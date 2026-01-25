2e0ef78e6e62f819302baf1857699d94a8629ff0
marcio-henriquehm <marcio.freitas@dcomp.ufs.br>
2026-01-25 17:59:03 -0300

    Parte 2 concluida

--- Minimax.py
+++ Minimax.py
@@ -58,6 +58,22 @@ detectam quando o sistema terminou
 avaliam a qualidade de um estado
 
 Essas funções não tomam decisões, apenas medem o estado.
+
+-Verificação de Estado Terminal
+Ideia:O sistema terminou quando todos os processos estão no estado concluído.
+
+
+-Função de Avaliação do Estado
+
+Avaliar o quão ruim ou bom está o escalonamento atual, considerando:
+
+tempo total acumulado
+
+atrasos ponderados pela prioridade
+
+Quanto maior o valor retornado, melhor o estado para o sistema (MAX).
+
+
 '''
 
 
@@ -68,13 +84,31 @@ def estado_terminal(estado):
     return True
 
 
+def funcao_avaliar(estado,lambd=1.0):
+    penalidade=0
+
+    for processo in estado.lista_processos:
+        atraso=processo.tempo_atual- processo.tempo_base
+        if atraso >0:
+            penalidade=penalidade+processo.prioridade*atraso
+        return -(estado.tempo_total +  lambd* penalidade)
+
+
 #testando criando objeto
 p1 = Processo(1, 5, 5, 2, "concluido")
 m1 = Maquina(1, "livre")
 estado = Estado([p1], [m1], 0)
+#testando funcionalidade 1(estado terminal)
 p1.estado="concluido"
 print(estado_terminal(estado))  # deve retornar True
 
+#testando funcionalidade 2( funcao avaliar)
+estado.tempo_total = 10
+p1.tempo_base = 5
+p1.tempo_atual = 7
+p1.prioridade = 2
+
+print(funcao_avaliar(estado))  # valor negativo, coerente com atraso
 
 
 
