782c22b5302955b0dddae82acbbd4e37e0a2b31a
marcio-henriquehm <marcio.freitas@dcomp.ufs.br>
2026-01-25 17:36:27 -0300

    implmentacao da  2 parte

--- Minimax.py
+++ Minimax.py
@@ -47,7 +47,38 @@ class Estado:
 
         self.tempo_total = tempo_total
 
+'''
+PARTE 2 — Funções Básicas do Sistema
+
+Objetivo desta parte
+Implementar funções independentes do Minimax, que:
+
+detectam quando o sistema terminou
+
+avaliam a qualidade de um estado
+
+Essas funções não tomam decisões, apenas medem o estado.
+'''
+
+
+''
+
+def estado_terminal(estado):
+    for processo in estado.lista_processos:
+        if processo.estado != "concluido":
+            return False
+    return True
+
+
 #testando criando objeto
-p1 = Processo(1, 5, 5, 2, "pendente")
+p1 = Processo(1, 5, 5, 2, "concluido")
 m1 = Maquina(1, "livre")
 estado = Estado([p1], [m1], 0)
+p1.estado="concluido"
+print(estado_terminal(estado))  # deve retornar True
+
+
+
+
+
+
