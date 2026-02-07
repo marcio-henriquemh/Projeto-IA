c9312ca2dead404b95e025a2da5a7c0f1864ab8b
Márcio Henrique Matos de Freitas <70753463+marcio-henriquemh@users.noreply.github.com>
2026-02-07 11:44:54 -0300

    Delete classe_maquina.py

--- classe_maquina.py
+++
@@ -1,9 +0,0 @@
-
-
-class Maquina:
-    #construtor
-    def __init__(self,id,estado,tempo_restante=0):       
-        self.id=id
-        self.estado=estado
-        self.tempo_restante=tempo_restante 
-      
